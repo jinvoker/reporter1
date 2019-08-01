7476ac4b8869743219e241b5fc5e00d4dee76eb6
jinvoker <python_powered@yahoo.com>
2019-08-01 21:54:11 +0530

    added Title and packages info.

--- reporter.sh
+++ reporter.sh
@@ -1,9 +1,13 @@
+echo "REPORTER : The Bash System Information Tool"
+echo "-------------------------------------------"
+
 kernel=$(uname -r)
 architecture=$(uname -m)
 host=$(uname -n)
 distro=$(lsb_release -d | cut -f 2 | cut -d" " -f -3) # Has to be worked out
 codename=$(lsb_release -d | cut -f 2 | cut -d" " -f 4)
 shell=$(echo $SHELL |cut -b 6-)" "$(echo $BASH_VERSION)
+packages=$(dpkg -l | wc -l)
 # Print the preprocessed system info.
 
 echo "KERNEL       :" \ $kernel 
@@ -12,3 +16,4 @@ echo "HOSTNAME     :" \ $host
 echo "DISTRO       :" \ $distro
 echo "CODENAME     :" \ $codename
 echo "SHELL        :" \ $shell
+echo "PACKAGES     :" \ $packages
