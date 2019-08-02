6687d90ddf4a01997ae4955ea87c105f4f4f42c2
jinvoker <python_powered@yahoo.com>
2019-08-03 00:50:10 +0530

    added uptime to reporter

--- reporter.sh
+++ reporter.sh
@@ -10,6 +10,7 @@ codename=$(lsb_release -d | cut -f 2 | cut -d" " -f 4)
 shell=$(echo $SHELL |cut -b 6-)" "$(echo $BASH_VERSION)
 packages=$(dpkg -l | wc -l)
 resolution=$(xdpyinfo | grep 'dimension' | cut -b 18-34)
+up=$(uptime | cut -b 13-18)
 
 # Print the preprocessed system info.
 echo""
@@ -23,4 +24,5 @@ echo "CODENAME     :" \ $codename
 echo "SHELL        :" \ $shell
 echo "PACKAGES     :" \ $packages
 echo "RESOLUTION   :" \ $resolution
+echo "UPTIME       :"\ $up
 echo""
