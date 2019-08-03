88fc7ed36e92629489f35d1ced4e4d9826206448
jinvoker <python_powered@yahoo.com>
2019-08-03 15:59:03 +0530

    fixed a display bug in uptime

--- reporter.sh
+++ reporter.sh
@@ -10,7 +10,7 @@ codename=$(lsb_release -d | cut -f 2 | cut -d" " -f 4)
 shell=$(echo $SHELL |cut -b 6-)" "$(echo $BASH_VERSION)
 packages=$(dpkg -l | wc -l)
 resolution=$(xdpyinfo | grep 'dimension' | cut -b 18-34)
-up=$(uptime | cut -b 13-19)
+up=$(uptime | cut -b 13-18)
 
 # Print the preprocessed system info.
 echo""
