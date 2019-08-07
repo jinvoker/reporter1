2466853908dbd90500f22e5ae7842b207a51552e
jinvoker <python_powered@yahoo.com>
2019-08-07 12:32:23 +0530

    Updated reporter for speed

--- reporter.sh
+++ reporter.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 echo "REPORTER : The Bash System Information Tool"
 echo "-------------------------------------------"
 machine=$(whoami)"@"$(uname -n)
@@ -5,14 +6,16 @@ machine=$(whoami)"@"$(uname -n)
 kernel=$(uname -r)
 architecture=$(uname -m)
 host=$(uname -n)
-distro=$(lsb_release -d | cut -f 2 | cut -d" " -f -3) # Has to be worked out
+distro=$(lsb_release -d | cut -f 2 | cut -d" " -f -3) 
 codename=$(lsb_release -d | cut -f 2 | cut -d" " -f 4)
 shell=$(echo $SHELL |cut -b 6-)" "$(echo $BASH_VERSION)
 packages=$(dpkg -l | wc -l)
 resolution=$(xdpyinfo | grep 'dimension' | cut -b 18-34)
-up=$(uptime | cut -b 13-18)
-memory="free-"$(free -h | grep Mem | cut -d" " -f 13)" / Used-"$(free -h | grep Mem | cut -d" " -f 21)
-cpu=$(cat /proc/cpuinfo | grep "model name" | head -n 1 | cut -c 13-70)
+up=$(uptime | cut -b 13-19)
+memory="Total-"$(free -h | grep Mem | cut -d" " -f 13)" / Used-"$(free -h | grep Mem | cut -d" " -f 21) # display bug in few distros.
+cpu=$(cat /proc/cpuinfo | grep "model name" | head -n 1 | cut -c 13-50)
+gpu=$(lspci | grep "Display controller" | cut -c 28-145)
+
 # Print the preprocessed system info.
 echo""
 echo "          "$machine
@@ -28,4 +31,5 @@ echo "RESOLUTION   :" \ $resolution
 echo "UPTIME       :"\ $up
 echo "RAM          :" \ $memory
 echo "CPU          :"\ $cpu
+echo "GPU          :"\ $gpu
 echo""
