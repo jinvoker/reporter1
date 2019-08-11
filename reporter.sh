3f885b49823774745097de0e2110c90e56ed0d23
jinvoker <python_powered@yahoo.com>
2019-08-11 21:50:16 +0530

    sped up

--- reporter.sh
+++ reporter.sh
@@ -6,7 +6,7 @@ machine=$(whoami)"@"$(uname -n)
 kernel=$(uname -r)
 architecture=$(uname -m)
 host=$(uname -n)
-distro=$(lsb_release -d | cut -f 2 | cut -d" " -f -3) 
+distro=$(lsb_release -d | cut -f 2 | cut -d" " -f -3)
 codename=$(lsb_release -d | cut -f 2 | cut -d" " -f 4)
 shell=$(echo $SHELL |cut -b 6-)" "$(echo $BASH_VERSION)
 packages=$(dpkg -l | wc -l)
@@ -19,7 +19,7 @@ gpu=$(lspci | grep "Display controller" | cut -c 28-145)
 echo""
 echo "          "$machine
 echo""
-echo "KERNEL       :" \ $kernel 
+echo "KERNEL       :" \ $kernel
 echo "ARCHITECTURE :" \ $architecture
 echo "HOSTNAME     :" \ $host
 echo "DISTRO       :" \ $distro
